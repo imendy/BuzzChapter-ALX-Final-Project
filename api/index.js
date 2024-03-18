1914618cedd8aeae3146281d4e7fef343f0fd04d
imendy <enmaju37@gmail.com>
2024-03-18 16:00:57 +0100

    feat: created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+// import postRoutes from './routes/post.route.js';
+// import commentRoutes from './routes/comment.route.js';
 
 
 dotenv.config();
@@ -18,9 +21,14 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+// app.use('/api/post', postRoutes);
+// app.use('/api/comment', commentRoutes);
