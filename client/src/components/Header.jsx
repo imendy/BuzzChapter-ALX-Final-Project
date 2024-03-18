fb1a3805905a65f02cd1281ae69a5bf1092b15f6
imendy <enmaju37@gmail.com>
2024-03-18 11:19:00 +0100

    feat: creating header elements

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,78 @@
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from 'react-icons/ai';
+// import { FaMoon, FaSun } from 'react-icons/fa';
+
+
+export default function Header() {
+  const gradientStyle = {
+    backgroundImage: "linear-gradient(to left, cyan, blue, purple)",
+    WebkitBackgroundClip: "text",
+    color: "transparent",
+  };
+
+  return (
+    <Navbar className='border-b-2'>
+      <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+        Bu
+        <span style={gradientStyle}>zz</span>
+        Chapter
+      </Link>
+
+      <form >
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+           className='hidden lg:inline'
+           sizing={'sm'}
+        //   value={searchTerm}
+        //   onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch  className='text-xl'/>
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 '
+          color='gray'
+          pill
+        //   onClick={() => dispatch(toggleTheme())}
+        >
+          {/* {theme === 'light' ? <FaSun /> : <FaMoon />} */}
+        </Button>
+
+        <Link to='/signin'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+
+          <Navbar.Toggle />
+        </div>
+
+        <Navbar.Collapse>
+        <Navbar.Link>
+                <Link to='/'>
+                   Home
+                </Link>
+            </Navbar.Link>
+
+            <Navbar.Link>
+                <Link to='/about'>
+                    About
+                
+                </Link>
+            </Navbar.Link>
+
+            <Navbar.Link>
+                <Link to='/projects'>
+                    Projects
+                
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
+    </Navbar>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin'),
+    // require('tailwind-scrollbar'),
+  ],
+};
