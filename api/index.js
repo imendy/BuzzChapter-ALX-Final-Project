f9c4c2c86c4d010827a13784d30eb362c1f91080
imendy <enmaju37@gmail.com>
2024-03-22 18:56:27 +0100

    feat: created comment section with edit-like-delete functionality

--- api/index.js
+++ api/index.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
-// import commentRoutes from './routes/comment.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 // import path from 'path';
+import newsletterRoutes from './routes/newsletter.route.js';
 
 
 dotenv.config();
@@ -34,7 +35,8 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
-// app.use('/api/comment', commentRoutes);
+app.use('/api/comment', commentRoutes);
+app.use('/api/newsletter', newsletterRoutes);
 
 
 app.use((err, req, res, next) => {
